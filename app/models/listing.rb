ac4ffa35f1d7db56b2fd3958f7c00890eda8aaee
Janice V. Dunn <janicedunn53@gmail.com>
2015-10-25 15:49:57 -0700

    Add image sizes to avatar and photo.

--- app/models/listing.rb
+++ app/models/listing.rb
@@ -1,6 +1,6 @@
 class Listing < ActiveRecord::Base
   belongs_to :user
-  has_attached_file :photo, :styles => { :medium => "300x300>", :thumb => "100x100>" }
+  has_attached_file :photo, :styles => { :medium => "300x300>", :thumb => "100x100>" }, :default_url => "/images/:style/missing_photo.png"
 
   validates :name, :presence => true
   validates :description, :presence => true
