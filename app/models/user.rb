ee599cf48675f3bd2bb3ce929378b91d67335b1a
Janice V. Dunn <janicedunn53@gmail.com>
2015-10-25 15:41:40 -0700

    Add and display avatar of user.

--- app/models/user.rb
+++ app/models/user.rb
@@ -5,6 +5,9 @@ class User < ActiveRecord::Base
          :recoverable, :rememberable, :trackable, :validatable
 
   has_many :listings
+  has_attached_file :avatar, :styles => { :medium => "300x300>", :thumb => "100x100>" }, :default_url => "/images/:style/missing.png"
+
   validates :username, :presence => true
   validates :neighborhood, :presence => true
+  validates_attachment_content_type :avatar, :content_type => /\Aimage\/.*\Z/
 end
