3c47671c857c824c315669b24bed7a3b36861691
Janice V. Dunn <janicedunn53@gmail.com>
2015-10-29 13:50:47 -0700

    Commit to switch back to master.

--- spec/models/listing_spec.rb
+++ spec/models/listing_spec.rb
@@ -1,7 +1,7 @@
 require 'rails_helper'
 
 describe Listing do
-  it { should belong_to :user }
+  it { should belong_to :listingable }
   it { should validate_presence_of :name }
   it { should validate_presence_of :description }
 end
